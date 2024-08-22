727dc1150f6d8f584e1bab397d99fe4390706515
SEG_DEV_AKESTUDIOS <elias.ake@akestudios.com>
2024-08-21 18:57:57 -0600

    tipos de datos typeScript

--- src/prueba.ts
+++ src/prueba.ts
@@ -1,1 +1,35 @@
-console.log("hola unjnj")
+console.log("Hola Afirme Seguros")
+
+
+let cotizacion: number;
+let cobertura = 20;
+let prima: number = 123.456;
+let poliza: bigint = 9007199254740991n;
+
+enum polizaEstatus {
+    EnProceso,
+    Terminada,
+    Cancelada
+}
+
+
+let estado: any = 10;
+
+estado = 19;      // ID
+estado = 'Nuevo Leon';   // texto
+
+let municipio: unknown = 10;
+municipio = 19001;
+municipio = 'Monterrey';
+
+
+let contacto: number | String;
+
+
+
+let nombre: string = "Afirme Seguros";
+contacto = 8114140667;
+let mensaje: string = `La poliza  de ${nombre} es ` + poliza + " y se encuentra con estatus " + polizaEstatus.Terminada 
++ " y fue generada en " + municipio + " ," + estado + " " + contacto;
+
+console.log(mensaje)
